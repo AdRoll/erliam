4571a09a69fa30ffa9d2517b2644ba77d99a0c7e
Brujo Benavides <fernando.benavides@adroll.com>
2020-04-14 16:29:56 +0200

    Add rebar3 formatting

--- src/erliam.erl
+++ src/erliam.erl
@@ -11,36 +11,27 @@
 %%% Created :  7 Apr 2017 by Mike Watters <mike.watters@adroll.com>
 -module(erliam).
 
-
--export([httpc_profile/0,
-         get_session_token/0,
-         credentials/0,
-         invalidate/0]).
-
+-export([httpc_profile/0, get_session_token/0, credentials/0, invalidate/0]).
 
 %% Return the current cached credentials (crash if none are cached or credential refresher
 %% server isn't running).
 credentials() ->
     erliam_srv:current().
 
-
 %% Fetch a new session token from STS (if aws_access_key and aws_secret_key app env values
 %% are set), or from instance metadata if not.  Normally external users don't need to call
 %% this; call credentials/0 instead.
 get_session_token() ->
     case {erliam_config:g(aws_access_key), erliam_config:g(aws_secret_key)} of
-        {AccessKeyId, SecretAccessKey} when AccessKeyId /= undefined;
-                                            SecretAccessKey /= undefined ->
-            erliam_sts:get_session_token(awsv4:long_term_credentials(AccessKeyId,
-                                                                     SecretAccessKey));
-        _ ->
-            imds:get_session_token()
+      {AccessKeyId, SecretAccessKey}
+          when AccessKeyId /= undefined; SecretAccessKey /= undefined ->
+          erliam_sts:get_session_token(awsv4:long_term_credentials(AccessKeyId, SecretAccessKey));
+      _ ->
+          imds:get_session_token()
     end.
-
 
 httpc_profile() ->
     erliam_config:g(httpc_profile, erliam).
-
 
 %% force cached credentials to be invalidated and refreshed.
 invalidate() ->
--- src/erliam_xml.erl
+++ src/erliam_xml.erl
@@ -12,47 +12,40 @@
 
 -include_lib("xmerl/include/xmerl.hrl").
 
-
 %%%% API
 
 parse(Xml) ->
     {E, _} = xmerl_scan:string(unicode:characters_to_list(Xml, utf8)),
     parse_element(E).
 
-
 %%%% INTERNAL FUNCTIONS
 
 parse_element(#xmlElement{name = Name, content = Content}) ->
-    {Name, case Content of
-               [#xmlElement{} | _] ->
-                   [{ChildName, parse_element(ChildContent)}
-                    || #xmlElement{name = ChildName,
-                                   content = ChildContent} <- Content];
-               _ ->
-                   parse_element(Content)
-           end};
-
+    {Name,
+     case Content of
+       [#xmlElement{} | _] ->
+           [{ChildName, parse_element(ChildContent)}
+            || #xmlElement{name = ChildName, content = ChildContent} <- Content];
+       _ ->
+           parse_element(Content)
+     end};
 parse_element([#xmlText{} | _] = L) ->
     case lists:all(fun (E) ->
-                           is_record(E, 'xmlText')
-                   end, L) of
-        true ->
-            unescape_xml_text(xmerl_xs:value_of(L));
-
-        %% if a list of elements is not solely text, discard the text elements and parse
-        %% the non-text elements:
-        false ->
-            parse_element([X
-                           || X <- L,
-                              not is_record(X, 'xmlText')])
+                           is_record(E, xmlText)
+                   end,
+                   L)
+        of
+      true ->
+          unescape_xml_text(xmerl_xs:value_of(L));
+      %% if a list of elements is not solely text, discard the text elements and parse
+      %% the non-text elements:
+      false ->
+          parse_element([X || X <- L, not is_record(X, xmlText)])
     end;
-
 parse_element(L) when is_list(L) ->
     [parse_element(X) || X <- L];
-
 parse_element(X) ->
     X.
-
 
 unescape_xml_text(X) ->
     unescape_xml_text(unicode:characters_to_binary(X, utf8),
@@ -64,28 +57,23 @@ unescape_xml_text(X) ->
 
 unescape_xml_text(X, []) ->
     X;
-unescape_xml_text(X, [{C, R}| T]) ->
+unescape_xml_text(X, [{C, R} | T]) ->
     unescape_xml_text(binary:replace(X, C, R, [global]), T).
-
 
 %%%% TESTS
 -ifdef(TEST).
+
 -include_lib("eunit/include/eunit.hrl").
 
 basic_decode_test() ->
-    Data = <<"<GetSessionTokenResponse xmlns=\"https://sts.amazonaws.com/doc/2011-06-15/\">
-  <GetSessionTokenResult>
-   <Credentials>
-    <AccessKeyId>ACCESS_KEY_ID</AccessKeyId>
-    <SecretAccessKey>SECRET_ACCESS_KEY</SecretAccessKey>
-    <SessionToken>SESSION_TOKEN</SessionToken>
-    <Expiration>EXPIRATION</Expiration>
-   </Credentials>
-  </GetSessionTokenResult>
-  <ResponseMetadata>
-   <RequestId>REQUEST_ID</RequestId>
-  </ResponseMetadata>
- </GetSessionTokenResponse>\n">>,
+    Data = <<"<GetSessionTokenResponse xmlns=\"https://sts.amazonaws.com/doc/2011-"
+             "06-15/\">\n  <GetSessionTokenResult>\n   <Credentials>\n   "
+             " <AccessKeyId>ACCESS_KEY_ID</AccessKeyId>\n    <SecretAccessKey>SECR"
+             "ET_ACCESS_KEY</SecretAccessKey>\n    <SessionToken>SESSION_TOKEN</Se"
+             "ssionToken>\n    <Expiration>EXPIRATION</Expiration>\n   </Credentia"
+             "ls>\n  </GetSessionTokenResult>\n  <ResponseMetadata>\n   <RequestId"
+             ">REQUEST_ID</RequestId>\n  </ResponseMetadata>\n </GetSessionTokenRe"
+             "sponse>\n">>,
     ?assertEqual({'GetSessionTokenResponse',
                   [{'GetSessionTokenResult',
                     [{'Credentials',
@@ -93,8 +81,7 @@ basic_decode_test() ->
                        {'SecretAccessKey', <<"SECRET_ACCESS_KEY">>},
                        {'SessionToken', <<"SESSION_TOKEN">>},
                        {'Expiration', <<"EXPIRATION">>}]}]},
-                   {'ResponseMetadata',
-                    [{'RequestId', <<"REQUEST_ID">>}]}]},
+                   {'ResponseMetadata', [{'RequestId', <<"REQUEST_ID">>}]}]},
                  parse(Data)).
 
 -endif.
